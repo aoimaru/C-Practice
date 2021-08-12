251105b50105a5a67fb003243b43a164c9a29e77
aoimaru <syabi2622aoi@icloud.com>
2021-08-13 02:55:00 +0900

    <array>

--- BasicSyntax/Container/BasicArray/after_array/after_array.cc
+++ BasicSyntax/Container/BasicArray/after_array/after_array.cc
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <array>
 
-
+using namespace std;
 
 int main(void) {
+	array<int, 5> x = {{0, 1, 2, 3, 4}};
+	auto size = x.size();
+	cout << size << endl;
+
+	for (int i=0; i < size; i++) {
+		cout << x[i] << endl;
+	}
+	
 	return 0;
 }
