84607258b36cd7dd1fcc5932e1b77c5e239d97d9
aoimaru <syabi2622aoi@icloud.com>
2021-08-13 11:58:47 +0900

    イテレータとset型

--- BasicSyntax/Container/BasicIteretors/basic_iteretors.cc
+++ BasicSyntax/Container/BasicIteretors/basic_iteretors.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -12,5 +13,34 @@ int main(void) {
 	cout << *it << endl;
 
 	cout << *(it+1) << endl;
+
+	cout << "Hello" << endl;
+
+	for (auto it=x.begin(); it != x.end(); it++) {
+		cout << *it << endl;
+	}
+	cout << "Hello2" << endl;
+
+	// イテレータはコンテナの種類に依存しないため, setでも動作する
+	set<int> y = {4, 3, 2, 1, 0};
+
+	for (auto it=y.begin(); it != y.end(); it++) {
+		cout << *it << endl;
+	}
+
+	// 出力は 0, 1, 2, 3, 4の順になる.なぜならset型だから
+
+	cout << "Hello3" << endl;
+
+	vector<int> z = {4, 3, 2, 1, 0};
+	for (auto it=z.begin(); it != z.end(); it++) {
+		cout << *it << endl;
+	}
+
+
+
+
+
+
 	return 0;
 }
