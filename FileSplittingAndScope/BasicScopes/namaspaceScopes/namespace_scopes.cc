a5deabcce5c004a789d8c041a70a867b0ce67fe1
aoimaru <syabi2622aoi@icloud.com>
2021-08-13 13:52:32 +0900

    入れ子名前空間

--- FileSplittingAndScope/BasicScopes/namaspaceScopes/namespace_scopes.cc
+++ FileSplittingAndScope/BasicScopes/namaspaceScopes/namespace_scopes.cc
@@ -8,10 +8,29 @@ namespace A {
 
 namespace B {
 	int count = 4;
+	int GetCount() {
+		return count;
+	}
+}
+
+// namaspaceは入れ子にできる
+namespace C {
+	int count = 6;
+	namespace P {
+		int count = 8;
+
+		int GetCount() {
+			return count;
+		}
+	}
 }
 
 int main(void) {
 	cout << A::count << endl;
 	cout << B::count << endl;
+	cout << "関数スコープ: " << B::GetCount() << endl;
+
+	cout << C::P::count << endl;
+	cout << C::P::GetCount() << endl;
 	return 0;
 }
